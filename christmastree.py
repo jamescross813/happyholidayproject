42bc2f84caf0e34a0e4dbb99d0bbd405a1514129
jamescross813 <crossjames813@gmail.com>
2022-12-23 22:52:20 -0600

    set up class

--- christmastree.py
+++ christmastree.py
@@ -1,32 +1,33 @@
 import random
-def christmastree():
-    print("      ^      ")
-    print("     / \     ")
-    print("    /___\    ")
-    print("   /     \   ")
-    print("  /_______\  ")
-    print("  /       \  ")
-    print(" /_________\ ")
-    print("     | |     ")
-    print("    |___|    ")
+class ChristmasTree:
+    def christmastree():
+        print("      ^      ")
+        print("     / \     ")
+        print("    /___\    ")
+        print("   /     \   ")
+        print("  /_______\  ")
+        print("  /       \  ")
+        print(" /_________\ ")
+        print("     | |     ")
+        print("    |___|    ")
 
-def seasongreeting():
-    greetings = ["Season's Greetings", "Happy Holidays", "Felicioutous Festivities"]
-    rand = random.randint(0,2)
-    print(greetings[rand])
+    def seasongreeting():
+        greetings = ["Season's Greetings", "Happy Holidays", "Felicioutous Festivities"]
+        rand = random.randint(0,2)
+        print(greetings[rand])
 
-def thingstosay(talk):
-    print(talk)
+    def thingstosay(talk):
+        print(talk)
 
-def getname():
-    name = input("what is your name?")
-    return name
+    def getname():
+        name = input("what is your name?")
+        return name
 
-def hohohofunction():
-    christmastree()
-    seasongreeting()
-    nameis = getname()
-    # print(nameis)
-    thingstosay("hello " + nameis)
+    def hohohofunction():
+        christmastree()
+        seasongreeting()
+        nameis = getname()
+        # print(nameis)
+        thingstosay("hello " + nameis)
 
-hohohofunction()
+    hohohofunction()
