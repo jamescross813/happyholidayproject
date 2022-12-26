ca36de3e94e8039a476ba115a267c6bd2c8cda80
jamescross813 <crossjames813@gmail.com>
2022-12-25 22:13:53 -0600

    set self.name variable

--- user.py
+++ user.py
@@ -2,6 +2,6 @@ class User:
     def __init__(self, name):
         self.name = name
 
-    def getname():
-        name = input("what is your name?")
-        return name
+    def getname(self):
+        self.name = input("what is your name?")
+        return self.name
