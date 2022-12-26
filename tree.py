e47c926dc41092c0ad9d08250b65dfccb73411a2
jamescross813 <crossjames813@gmail.com>
2022-12-25 22:07:07 -0600

    moved print christmas tree function to tree class

--- tree.py
+++ tree.py
@@ -1,2 +1,13 @@
 class Tree:
-    pass
+    def printchristmastree():
+        print("      ^      ")
+        print("     / \     ")
+        print("    /___\    ")
+        print("   /     \   ")
+        print("  /_______\  ")
+        print("  /       \  ")
+        print(" /_________\ ")
+        print("     | |     ")
+        print("    |___|    ")
+
+    
