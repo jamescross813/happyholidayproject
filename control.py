71f402b3600a45bb1580aaabf421c1ad9c2ee8d0
jamescross813 <crossjames813@gmail.com>
2022-12-26 19:21:49 -0600

    calling greeting season greeting function

--- control.py
+++ control.py
@@ -1,9 +1,11 @@
 from user import User
 from tree import Tree
+from greeting import Greeting
 
 class Control(Tree):
     Tree.printchristmastree()
     User.getname()
+    Greeting.seasongreeting()
 # want to call christmas tree print function
 # want to call get name function
 # want to call season greeting function
