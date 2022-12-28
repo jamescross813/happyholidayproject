93494aa1293a1b9a5b032efdcfb32447e6e561e2
jamescross813 <crossjames813@gmail.com>
2022-12-27 22:50:24 -0600

    expand greetings list

--- greeting.py
+++ greeting.py
@@ -4,7 +4,12 @@ from user import User
 class Greeting:
     greetings = ["Season's Greetings", 
         "Happy Holidays", 
-        "Felicioutous Festivities"]
+        "Felicioutous Festivities",
+        "Comfort and Joy",
+        "Warmest Wishes",
+        "Happy Yultide",
+        "Be Merry",
+        "Glad Tidings"]
 
     def seasongreeting():
         rand = random.randint(0,2)
