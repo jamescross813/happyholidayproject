b3d3e345b7c524641c4e2e2a05341440d819e21e
jamescross813 <crossjames813@gmail.com>
2023-01-23 23:49:36 -0600

    bug hunt

--- checker.py
+++ checker.py
@@ -1,18 +1,20 @@
 class Checker:
-    def __init__(self, state):
-        self.state = state
+    def __init__(self, behave):
+        self.behave = behave
 
     def ask_question(self):
-        self.state = input("Have you been Naughty or Nice?")
-        return self.check_naughty_nice(self.state)
+        self.behave = input("Have you been Naughty or Nice?")
+        return self.check_naughty_nice(self.behave)
 
-    def check_naughty_nice(self):
-        if self.state.lower() =="nice":
-            print("good job!")
-            return True
-        elif self.state.lower() == "naughty":
-            print("uh oh")
-            return False
-        else:
-            print("Error with answer. Use Naughty or Nice only.")
-            return self.ask_question()
+    def check_naughty_nice(behave):
+        print(behave)
+        # status = self.status
+        # if status.lower() =="nice":
+        #     print("good job!")
+        #     return True
+        # elif status.lower() == "naughty":
+        #     print("uh oh")
+        #     return False
+        # else:
+        #     print("Error with answer. Use Naughty or Nice only.")
+        #     return self.ask_question()
