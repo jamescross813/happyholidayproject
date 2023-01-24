e8f4806845872e9e1e2e5a2a29978578992b35ad
jamescross813 <crossjames813@gmail.com>
2023-01-23 23:33:22 -0600

    import checker class

--- control.py
+++ control.py
@@ -1,11 +1,13 @@
 from user import User
 from tree import Tree
 from greeting import Greeting
+from checker import Checker
 
 class Control(Tree):
     Tree.printchristmastree()
     User.getname(User)
     Greeting.seasongreeting()
+
 # want to call christmas tree print function
 # want to call get name function
 # want to call season greeting function
