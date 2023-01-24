6965ebcaf23b6c7852ef2c8d377b5393fdd48361
jamescross813 <crossjames813@gmail.com>
2023-01-23 23:36:03 -0600

    input for ask_q

--- checker.py
+++ checker.py
@@ -1,7 +1,10 @@
 class Checker:
-    def ask_question():
-        return "Have you been Naughty or Nice?"
-        
+    
+
+    def ask_question(self):
+        self.state = input("Have you been Naughty or Nice?")
+        return self.state
+
     def check_naughty_nice(ans):
         if ans=="yes":
             return True
