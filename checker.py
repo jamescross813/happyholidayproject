a58bc27fe601ab48b2e3bf39644971bee4561766
jamescross813 <crossjames813@gmail.com>
2023-01-25 20:33:42 -0600

    removed bug checker

--- checker.py
+++ checker.py
@@ -8,10 +8,8 @@ class Checker:
 
     def check_naughty_nice(self, behave):
         if behave.lower() =="nice":
-            print("good job!")
             return True
         elif behave.lower() == "naughty":
-            print("uh oh")
             return False
         else:
             print("Error with answer. Use Naughty or Nice only.")
