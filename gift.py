5d60af5c9530faac15f0542d0644d48956df46dd
jamescross813 <crossjames813@gmail.com>
2023-01-29 22:08:32 -0600

    gift func moved

--- gift.py
+++ gift.py
@@ -3,4 +3,10 @@ class Gift:
     def __init__(self, presents):
         self.presents =["doll", "car", "ball", "orange"]
 
-    
+    def gift(bool, self):
+            if bool==False:
+                return "lump of coal"
+            else:
+                end = len(self.present)-1
+                rand = random.randint(0,end)
+                return "Your gift this year is: " + self.present[rand]
