15ccd11987210fdaf6089e4f7bdc498c96fcc01f
jamescross813 <crossjames813@gmail.com>
2023-02-08 22:30:04 -0600

    comment out unnec code

--- checker.py
+++ checker.py
@@ -18,10 +18,10 @@ class Checker:
             print("Error with answer. Use Naughty or Nice only.")
             return self.ask_question(self)
 
-    def gift(bool, self):
-        if bool==False:
-            return "lump of coal"
-        else:
-            end = len(self.present)-1
-            rand = random.randint(0,end)
-            return "Your gift this year is: " + self.present[rand]
+    # def gift(bool, self):
+    #     if bool==False:
+    #         return "lump of coal"
+    #     else:
+    #         end = len(self.present)-1
+    #         rand = random.randint(0,end)
+    #         return "Your gift this year is: " + self.present[rand]
