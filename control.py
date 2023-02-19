cecc9c71dbb25fd973b8feebcf109318de0d3166
jamescross813 <crossjames813@gmail.com>
2023-02-18 22:13:45 -0600

    tidy up of minor issues in function calling

--- control.py
+++ control.py
@@ -3,11 +3,11 @@ from tree import Tree
 from greeting import Greeting
 from checker import Checker
 
-class Control(Tree):
+class Control():
     Tree.printchristmastree()
     User.getname(User)
     Greeting.seasongreeting()
-    Checker.ask_question(Checker)
+    print(Checker.ask_question(Checker))
 # want to call christmas tree print function
 # want to call get name function
 # want to call season greeting function
