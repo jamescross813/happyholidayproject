f8f33f12ce216951cd040dd2c09e53d8e5eeb49a
jamescross813 <crossjames813@gmail.com>
2023-02-18 22:05:38 -0600

    chasing down self bug issues

--- gift.py
+++ gift.py
@@ -8,4 +8,4 @@ class Gift:
             else:
                 end = len(Gift.presents)-1
                 rand = random.randint(0,end)
-                return "Your gift this year is: " + self.present[rand]
+                return "Your gift this year is: " + Gift.present[rand]
